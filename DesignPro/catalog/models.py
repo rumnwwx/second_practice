8f837189288eea2714cbfa3337ef1f7c8b36a730
rumnwwx <elchaninapolina@gmail.com>
2024-11-14 11:42:53 +0700

    2.2 done!

--- DesignPro/catalog/models.py
+++ DesignPro/catalog/models.py
@@ -1,3 +1,5 @@
+from cProfile import label
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -20,6 +22,7 @@ class CustomUser(AbstractUser):
 
 class Category(models.Model):
     name=models.CharField(max_length=150)
+
 
     def __str__(self):
         return self.name
